c89c161e4d79aeb35ff3538a7d316e71f2cb35c5
Anand Kumar Mishra <akm@secpanel.com>
2013-02-06 18:08:33 +0000

    curl bash script

--- curl/sample-curl.sh
+++ curl/sample-curl.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-curl https://apis.secpanel.com/rouge-ips-json.php?key=your_key\&limit=5\&offset=50 > ip-list.json
+#Setting the default values for optional arguments
+limit=10 # default limit
+offset=0 # default offset
+
+#Subroutine to print coreect usage, in case of validiation failure
+usage(){
+    echo "Usage: $0 api-key [limit] [offset]"
+    exit
+}
+
+if [ $# -lt 1 ]
+ then
+    usage 
+ elif [ $# -eq 1 ]; then
+  #Seems key is supplied
+  api_key="$1"
+ elif [ $# -eq 2 ]; then
+  #Seems key as well as limit is supplied
+  api_key="$1"
+  limit="$2"
+ elif [ $# -eq 3 ]; then
+  #Seems key,limit and offset value is also supplied
+  api_key="$1"
+  limit="$2"
+  offset="$3"
+ else
+  #More than required arguments supplied
+  usage
+ fi
+
+curl https://apis.secpanel.com/rouge-ips-json.php?key=$api_key\&limit=$limit\&offset=$offset > response.json
+
+cat response.json
